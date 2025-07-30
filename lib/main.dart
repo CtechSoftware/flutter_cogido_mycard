034db13f75d00d50ef5e58966190f22a2686eaa1
Elvis Suarez Ramos <admin@camilatech.com>
2025-07-30 00:03:47 -0500

    Primer ejemplo de Aplicación relizada en Flutter utilizando os diferentes Widgets disponibles en la libreria material.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-//Stateless vs Stateful
 void main() {
   runApp(MyApp());
 }
@@ -8,50 +7,105 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String nombre = 'Pedro Montes';
-  int contador = 100;
-  int contador1 = 100;
-  double tamano = 20.0;
-  Color colorLetra = Colors.red;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text('"Flutter State"'),
-      ),
-      body: Center(
-        child: Column(
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(contador1.toString(), style: TextStyle(fontSize: 40.0)),
-            Text(contador.toString(), style: TextStyle(fontSize: 40.0)),
-            Text(
-              "Nombre: $nombre",
-              style: TextStyle(fontSize: tamano, color: colorLetra),
+            CircleAvatar(
+              radius: 56,
+              backgroundColor: Colors.white54,
+              backgroundImage: AssetImage('assets/images/Elvis.jpg'),
             ),
-
-            ElevatedButton(
-              child: Text("Cambiar texto"),
-              onPressed: () {
-                nombre = 'Ramon Gonzales';
-                contador = contador + 1;
-                contador1 = contador - 1;
-                tamano = tamano + 1;
-                colorLetra = Colors.blue;
-                setState(() {});
-              },
+            Text(
+              'Elvis Suárez Ramos',
+              style: TextStyle(
+                fontFamily: 'Lobster',
+                fontSize: 26.0,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+                letterSpacing: 3.0,
+              ),
+            ),
+            SizedBox(height: 4.0),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white60,
+                letterSpacing: 3.0,
+              ),
+            ),
+            SizedBox(
+              width: 170.0,
+              child: Divider(
+                color: Colors.white60,
+                height: 20.0,
+                thickness: 1.0,
+                indent: 20.0,
+                endIndent: 20.0,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              elevation: 5.0,
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.indigo),
+                trailing: Icon(
+                  Icons.check_circle_outline_outlined,
+                  color: Colors.indigo,
+                ),
+                title: Text(
+                  '+51 966 333 347',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  'Teléfono',
+                  style: TextStyle(fontSize: 14.0, color: Colors.black54),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              elevation: 5.0,
+              child: ListTile(
+                leading: Icon(Icons.mail, color: Colors.indigo),
+                trailing: Icon(
+                  Icons.check_circle_outline_outlined,
+                  color: Colors.indigo,
+                ),
+                title: Text(
+                  'elvis240877@gmail.com',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  'Correo electrónico',
+                  style: TextStyle(fontSize: 14.0, color: Colors.black54),
+                ),
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/facebook.png', height: 60),
+                SizedBox(width: 30.0),
+                Image.asset('assets/images/instagram.png', height: 60),
+                SizedBox(width: 30.0),
+                Image.asset('assets/images/twitter.png', height: 60),
+              ],
             ),
           ],
         ),
